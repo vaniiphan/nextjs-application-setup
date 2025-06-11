0abef333329ee20f5f24a0b0f27dab60154d01b1
vaniiphan <vanphan3101@gmail.com>
2025-06-11 21:18:33 +0700

    Add .next/static/chunks/src_app_page_tsx_b2d26e7c._.js 1749651511451

---
+++ .next/static/chunks/src_app_page_tsx_b2d26e7c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2bd162b4._.js",
+  "static/chunks/src_d0de5a7f._.js"
+],
+    source: "dynamic"
+});
